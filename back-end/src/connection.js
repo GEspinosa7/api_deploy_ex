d697304c5778dbce88ba96477b498f49f3741014
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2021-06-29 16:00:57 -0300

    database heroku  config

--- back-end/src/connection.js
+++ back-end/src/connection.js
@@ -1,10 +1,14 @@
 const knex = require('knex')({
    client: 'pg',
    connection: {
-      host: 'localhost',
-      user: 'postgres',
-      password: '123456',
-      database: 'market_cubos'
+      host: 'ec2-52-23-45-36.compute-1.amazonaws.com',
+      user: 'jtfujhrqsfaxhj',
+      password: '9116348f7762156f66971fb7ef49e9da2f2359ab028b0f4ecaaf6ca0f7d66143',
+      database: 'desl4851dot0p8',
+      port: 5432,
+      ssl: {
+         rejectUnauthorized: false
+      }
    }
 });
 
