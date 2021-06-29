491cb2e6531fe6d02689b68ea929e6ad6cdb6250
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2021-06-29 16:39:16 -0300

    fix email validation

--- src/validations/email.js
+++ src/validations/email.js
@@ -2,7 +2,7 @@ const knex = require('../connection');
 
 const validarEmail = async (email) => {
    const usuario = await knex('usuarios').where('email', email);
-   if (usuario) return 'Este email ja está cadastrado';
+   if (usuario.length > 0) return 'Este email ja está cadastrado';
 };
 
 module.exports = validarEmail;
